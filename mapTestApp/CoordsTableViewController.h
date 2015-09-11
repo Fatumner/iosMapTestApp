b239d6576375697ab2b93ad1f6fbfea4707c99ba
Mateusz Florczak <mateuszf@AIRMEPHIT.local>
2015-09-11 10:53:30 +0200

    working project with empty main views and mapKit

---
+++ mapTestApp/CoordsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoordsTableViewController.h
+//  mapTestApp
+//
+//  Created by Mateusz Florczak on 11/09/15.
+//  Copyright (c) 2015 Kainos Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoordsTableViewController : UITableViewController
+
+@end
---
+++ mapTestApp/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  mapTestApp
+//
+//  Created by Mateusz Florczak on 11/09/15.
+//  Copyright (c) 2015 Kainos Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
